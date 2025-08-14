3f6edfb933e5391d5ebc113a9a445b766afa6cf9
Jonathan Freeman <jonathan.freeman@selu.edu>
2025-08-14 15:00:40 -0500

    colors and better drawing

--- whiteboard/node_2d.gd
+++ whiteboard/node_2d.gd
@@ -1,13 +1,49 @@
 extends Node2D
 
-var mi_pos:Array = []
+var color: Color = Color.BLACK
+var strokes: Array = []
+var has_last_pos: bool = false
+var last_pos: Vector2
 
-func _input(event:InputEvent):
- if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-  return
- mi_pos.append(event.position)
- queue_redraw()
+func _on_black_color_pressed() -> void:
+	color = Color.BLACK
 
-func _draw():
- for point in mi_pos:
-  draw_circle(point,5,Color.BLACK)
+func _on_white_color_pressed() -> void:
+	color = Color.WHITE
+
+func _on_blue_color_pressed() -> void:
+	color = Color.BLUE
+
+func _on_green_color_pressed() -> void:
+	color = Color.GREEN
+
+func _on_red_color_pressed() -> void:
+	color = Color.RED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				last_pos = event.position
+				has_last_pos = true
+				strokes.append({"pos": last_pos, "color": color})
+				queue_redraw()
+			else:
+				has_last_pos = false
+
+	elif event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if has_last_pos:
+			var distance = last_pos.distance_to(event.position)
+			var steps = int(distance / 2) 
+			for i in range(steps):
+				var t = float(i) / steps
+				var interp_pos = last_pos.lerp(event.position, t)
+				strokes.append({"pos": interp_pos, "color": color})
+		strokes.append({"pos": event.position, "color": color})
+		last_pos = event.position
+		has_last_pos = true
+		queue_redraw()
+
+func _draw() -> void:
+	for stroke in strokes:
+		draw_circle(stroke.pos, 5, stroke.color)
